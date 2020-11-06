c46014b3640960aab9cda7d9c0fb019852e9a885
Diego Avedissian <61209163+davedissian@users.noreply.github.com>
2020-11-06 20:14:41 -0300

    Adicionado itens no body

--- home.php
+++ home.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Newsletter</title>
+    <title>Home</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="estilo.css">
 </head>
@@ -16,11 +16,20 @@
     ?>
 
     <br>
-    <div class="container text-center">
-        <img src="./img/home.jpeg" width="250px" alt="">
-      
-    </div>
-    <br>
+    <div class="container">
+        <br>
+        <div class="col">
+            <div class="row">
+                <br>
+                <div>
+                <h5>Descartar seus resíduos de forma ecológica e consciente.</h5>
+                <a class="home" href="pontoscoleta.php">Encontre um ponto de coleta</a>
+                </div>
+
+                <img src="./img/home.png" width="320px" alt="">
+        
+            </div>
+        </div>
 
 </body>
 
