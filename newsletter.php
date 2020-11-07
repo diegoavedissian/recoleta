07c6fddcac2edf33c7a9d7298762fbae50042902
Diego Avedissian <61209163+davedissian@users.noreply.github.com>
2020-11-06 23:13:31 -0300

    Texto novo

--- newsletter.php
+++ newsletter.php
@@ -16,10 +16,21 @@
     ?>
 
     <br>
-    <div class="container text-center">
-        <img src="./img/save.png" width="250px" alt="">
-        <h2>Lorem ipsum dolor.</h2>
-        <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora error voluptate, minus molestiae, quos quae necessitatibus veritatis esse libero accusamus, quas autem aut doloribus earum maxime incidunt dolorem est reprehenderit? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium maxime nihil autem vero doloribus quasi vitae perferendis unde beatae? Iste itaque eius vero alias accusantium laudantium dignissimos commodi quibusdam quaerat.</h3>
+    <div class="container">
+        <center><img src="./img/save.png" width="250px"><br></center>
+        <br>
+        <h2>O sonho de conquistar lixo zero É POSSÍVEL!</h2><br>
+        <h3>Abrace esta causa</h3><br>
+        <h3>
+            Nos deparamos com bairros como Bom Jesus em Guarulhos com uma situação tão precária e alarmante, total falta de saneamento básico, dignidade, respeito para com os moradores, por isso é importantíssimo que estas ações abaixo citadas se tornem realidade, saiam do papel, ou mesmo tome proporções maiores do que temos hoje!
+
+            É maravilhoso poder morar num ambiente onde o todo é saudável, seu lar, seus vizinhos, as ruas, seu trabalho, juntos cooperando para o bem estar do planeta.
+
+            Há 16 anos Blangadesh na África, baniu as sacolas plásticas e está na liderança deste banimento, na época a situação ficou alarmante e exigiu medidas drásticas.
+
+            São Francisco vai mais além, pois buscam na raiz do problema, trazer a solução para resíduo zero. O qual nos inspira e ajuda a responder - O que é capaz de mudar uma cultura?
+
+            Iniciativas que lá já são realidade, começam a dar seus primeiros passos no Brasil, como incluir na educação infantil disciplinas com conceitos que levam pra vida - do cuidado e bem estar consigo, o próximo e o planeta, educação nas empresas a união entre produção e descarte gera a conversa e ações de novos conceitos como Indústria 4.0 que dentre outros proporciona o reaproveitamento total entre indústrias, fábricas, empresas... num ciclo harmônico onde tudo se reaproveita, mais pontos de coleta espalhados por todo lugar nas praças, feiras, mercados, ruas... e diferente do que ocorre aqui no Brasil que necessita de um crescimento na área da reciclagem, sendo que do total descartado menos de 2% são reciclados ou reaproveitados, deve buscar proporcionar mais incentivos para o aumento na reciclagem, San Francisco vivencia as coletas seletivas, as quais realmente destinam os materiais para fins mais produtivos, todas estas são ações maravilhosas inicialmente impostas e hoje prazerosas no dia a dia dos são franciscanos, vamos trilhar este caminho.</h3>
     </div>
     <br>
 
